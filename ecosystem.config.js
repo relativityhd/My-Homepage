0151c6515b0b09746432e71e7a406cd00d61d284
Tobias Hölzer <tobiashoelzer@hotmail.com>
2022-10-17 22:40:02 +0200

    Change GH-Action for own server-hosting with pm2

---
+++ ecosystem.config.js
@@ -0,0 +1,38 @@
+module.exports = {
+  apps: [
+    {
+      name: 'My-Homepage',
+      script: 'npm start',
+      time: true,
+      instances: 1,
+      autorestart: true,
+      max_restarts: 50,
+      watch: false,
+      max_memory_restart: '500M',
+      env: {
+        PORT: 3000,
+        MONGO_PASS: process.env.MONGO_PASS,
+        MONGO_USER: process.env.MONGO_USER,
+        MONGO_URL: process.env.MONGO_URL
+      },
+    },
+  ],
+  deploy: {
+    production: {
+      user: 'runner',
+      host: 'tobiashoelzer.de',
+      key: '~/.ssh/github_actions_rsa',
+      ref: 'origin/main',
+      repo: 'https://github.com/relativityhd/my-homepage',
+      path: '/var/www/my-homepage',
+      'post-deploy': 'npm install && pm2 save',
+      env: {
+        NODE_ENV: 'production',
+        PORT: 3800,
+        MONGO_PASS: process.env.MONGO_PASS,
+        MONGO_USER: process.env.MONGO_USER,
+        MONGO_URL: process.env.MONGO_URL
+      },
+    },
+  },
+}
